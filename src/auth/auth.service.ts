54140207d1a1ca0ed37a4f3b7ff155357f343592
Dollars Kenthouky <dollarskenthouky@gmail.com>
2023-08-15 19:13:14 +0100

    sum

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class AuthService {
+	constructor(
+		private jwtService: JwtService,
+		@InjectModel('User') private readonly userModel: Model<User>,
+		) {}
+
+	async validateUser(username: string, password: string): Promise<any> {
+		const user = await this.userModel.findOne({ username });
+
+		if (user && (await bcrypt.compare(password, user.password))) {
+			return user;
+		}
+
+		return null;
+	}
+
+	async generateToken(user: any) {
+		const payload = { sub: user.id };
+		return {
+			access_token: this.jwtService.sign(payload),
+		};
+	}
+}
